5065c7bd215d1b710bce6e6e192ad4e2c811c4c6
Haidar Ali <haidar47x@Haidars-MacBook-Pro.local>
2024-12-07 23:05:48 +0500

    Updated dark mode icons

--- themes/tufte-custom/static/js/main.js
+++ themes/tufte-custom/static/js/main.js
@@ -1,6 +1,6 @@
 function toggleStyle(mode = null) {
     const htmlElem = document.documentElement;
-    const style = "filter: invert(1) contrast(0.95) brightness(0.55) saturate(1) sepia(0.35);";
+    const style = "filter: invert(1) contrast(0.9) brightness(0.75) saturate(1) sepia(0.35);";
 
     if (mode === null) {
         mode = localStorage.getItem('mode') === 'dark' ? 'light' : 'dark';
@@ -8,14 +8,18 @@ function toggleStyle(mode = null) {
 
     if (mode === 'dark') {
         htmlElem.style.cssText += style;
-        localStorage.setItem('mode', 'dark');
+        document.querySelector('.appearance img').classList.replace('light', 'dark');
+        document.querySelector('.appearance img').src = "img/moon.png"
     } else {
         htmlElem.style.cssText = htmlElem.style.cssText.replace(style, "");
-        localStorage.setItem('mode', 'light');
+        document.querySelector('.appearance img').classList.replace('dark', 'light');
+        document.querySelector('.appearance img').src = "img/sun.png"
     }
+
+    localStorage.setItem('mode', mode);
 }
 
-document.querySelector(".appearance button").addEventListener("click", () => {
+document.querySelector(".appearance").addEventListener("click", () => {
     toggleStyle();
 });
 
