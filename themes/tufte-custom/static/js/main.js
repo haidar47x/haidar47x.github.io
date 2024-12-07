bcba280d1153eb10b81b5e95e55a7626718e2864
Haidar Ali <haidar47x@Haidars-MacBook-Pro.local>
2024-12-07 20:15:35 +0500

    Enhanced dark mode functionality

--- themes/tufte-custom/static/js/main.js
+++ themes/tufte-custom/static/js/main.js
@@ -1,9 +1,21 @@
-document.querySelector(".appearance button").addEventListener("click", () => {
+function toggleStyle() {
+    let mode = localStorage.getItem('mode') || 'light';
+
     const htmlElem = document.documentElement;
     const style = "filter: invert(1) contrast(0.95) brightness(0.55) saturate(1) sepia(0.35);"
-    if (htmlElem.style.cssText.includes(style)) {
-        htmlElem.style.cssText = htmlElem.style.cssText.replace(style, "");
-    } else {
+    if (mode === 'light') {
         htmlElem.style.cssText += style;
+        localStorage.setItem('mode', 'dark');
+    } else {
+        htmlElem.style.cssText = htmlElem.style.cssText.replace(style, "");
+        localStorage.setItem('mode', 'light');
     }
+}
+
+document.querySelector(".appearance button").addEventListener("click", () => {
+    toggleStyle();
 });
+
+(function() {
+    toggleStyle();
+})();
