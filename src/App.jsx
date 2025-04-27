53251e55c8b0da01d95a0b270a06dbe072620812
Haidar Ali <haidar47x@gmail.com>
2025-04-28 00:07:42 +0500

    setup index.html and add required fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import About from "./components/pages/About"
+
 function App() {
 
   return (
     <div class="container">
-      <h1 class="text-black">welcome to the maker's den</h1>
+      <About />
     </div>
   )
 }
