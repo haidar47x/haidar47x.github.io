902bb90a1acec118afbdd837316100062fd2ea85
Haidar Ali <haidar47x@gmail.com>
2025-04-28 20:13:59 +0500

    Created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import About from "./components/pages/About"
+import Navbar from "./components/partials/Navbar"
 
 function App() {
 
   return (
-    <div class="container">
-      <About />
+    <div className="site-container">
+      <Navbar />
+      <main class="content">
+        <About />
+      </main>
     </div>
   )
 }
