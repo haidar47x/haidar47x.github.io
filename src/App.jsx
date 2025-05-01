2101dffb7f79f60983216ca99ed55059355ed55d
Haidar Ali <haidar47x@gmail.com>
2025-05-02 02:22:33 +0500

    Added basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import About from "./components/pages/About";
+import Posts from "./components/pages/Posts";
+import Projects from "./components/pages/Projects";
+
 import Footer from "./components/partials/Footer";
 import Navbar from "./components/partials/Navbar";
 
 function App() {
   return (
-    <div className="site-container">
-      <Navbar />
-      <main className="content text-2xl">
-        <About />
-      </main>
-      <Footer />
-    </div>
+    <Router basename={import.meta.env.BASE_URL ?? "/"}>
+      <div className="site-container">
+        <Navbar />
+        <main className="content text-2xl">
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
