df0574f72c4cb75c1151e2ff767ef8e8705129ad
Haidar Ali <haidar47x@gmail.com>
2025-05-02 22:48:21 +0500

    Articles/Article refactored

--- src/components/pages/Articles.jsx
+++ src/components/pages/Articles.jsx
@@ -1,25 +1,10 @@
 import React from "react";
 import gradientImage from "../../assets/gradient.png";
-import postsData from "../../assets/posts.json";
-
-const Separator = ({ year }) => (
-  <div className="eb-garamond flex w-full items-center justify-center py-4 text-xl sm:text-2xl md:text-3xl">
-    <div className="flex-grow border-t border-stone-400"></div>
-    <span className="px-4 font-light text-gray-600">{year}</span>
-    <div className="flex-grow border-t border-stone-400"></div>
-  </div>
-);
+import Article from "./sections/Article";
+import { posts, firstYear } from '../../utils'
 
 function Articles() {
-  const posts = postsData.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-  const dateFormat = Intl.DateTimeFormat("en-US", {
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-  });
-
-  let currentYear = new Date(posts[0]?.date).getFullYear();
+  let separatorYear = firstYear;
 
   return (
     <div className="relative flex flex-col items-center gap-12 overflow-x-hidden px-4 pt-24 pb-4 md:gap-32 md:py-24">
@@ -34,31 +19,13 @@ function Articles() {
         </h1>
         <div className="posts flex flex-col gap-5">
           {posts.map((post, i) => {
-            const postYear = new Date(post?.date).getFullYear();
-            let printSeparator = null;
-            if (postYear !== currentYear) {
-              currentYear = postYear;
+            const postYear = new Date(post.date).getFullYear();
+            let printSeparator = false
+            if (postYear !== separatorYear) {
+              separatorYear = postYear;
               printSeparator = true;
             }
-            return (
-              <React.Fragment key={i}>
-                {printSeparator ? (
-                  <Separator year={currentYear} className="" />
-                ) : null}
-                <div className="entry flex flex-col justify-start gap-1 pl-0.5">
-                  <div className="eb-garamond text-sm font-semibold text-stone-600 italic md:text-base">
-                    <time dateTime={post.date}>
-                      {dateFormat.format(new Date(post.date))}
-                    </time>
-                  </div>
-                  <h3 className="text-sm duration-100 hover:text-orange-500 sm:text-base md:text-lg">
-                    <a href={post.url} target="_blank">
-                      {post.title}
-                    </a>
-                  </h3>
-                </div>
-              </React.Fragment>
-            );
+            return <Article key={i} post={post} separatorYear={separatorYear} printSeparator={printSeparator} />
           })}
         </div>
       </div>
