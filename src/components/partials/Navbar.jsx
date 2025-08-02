27624cd596e5e93fc9e3547cbdb8aa7eca3f82e6
Haidar Ali <haidar47x@gmail.com>
2025-08-02 18:54:41 +0500

    Visual tuning for fonts and navbar

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -6,7 +6,7 @@ import SocialLinks from "./components/SocialLinks";
 function Navbar() {
   return (
     <div className="navbar sticky top-5 z-10 mt-5 flex flex-nowrap items-center justify-center bg-transparent">
-      <div className="static flex h-12 items-center justify-center gap-4 rounded-[0.6rem] bg-white/80 px-2 font-light shadow-[0_1px_rgba(0,0,0,0.14)] backdrop-blur-[9.6px] dark:bg-slate-800/75 dark:text-slate-300 dark:shadow-xs/25 dark:border-1 dark:border-slate-700/60 tracking-tight">
+      <div className="skeumorphic-borders static flex h-12 items-center justify-center gap-4 rounded-[0.6rem] bg-white/80 px-2 font-light shadow-[0_1px_rgba(0,0,0,0.14)] backdrop-blur-[9.6px] dark:bg-slate-800/75 dark:text-slate-300 dark:shadow-xs/25 tracking-tight">
         <PageLinks />
         <Separator />
         <SocialLinks />
