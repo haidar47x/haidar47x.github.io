a3cf22cc35502e426ff74f53b7cba1fa62dd3391
Haidar Ali <haidar47x@gmail.com>
2025-08-05 21:32:14 +0500

    Button improvements

--- src/components/partials/components/ConnectLinks.jsx
+++ src/components/partials/components/ConnectLinks.jsx
@@ -8,14 +8,14 @@ function ConnectLinks() {
   const whileHover = { y: 1, x: 1 };
   return (
     <div className="flex flex-row justify-center gap-3 md:gap-2 pr-1 md:p-0">
-      <motion.button className="rounded-[0.6rem] shadow-md bg-black/5 dark:bg-[rgba(255,255,255,0.08)] px-2.5 py-1 backdrop-blur-[9.6px] border-t-slate-500/75 border-1 border-slate-700"
+      <motion.button className="rounded-[0.6rem] shadow-md bg-black/5 dark:bg-[rgba(255,255,255,0.08)] px-2.5 py-1 backdrop-blur-[9.6px] border-t-slate-500/60 border-1 border-slate-700"
                      whileHover={whileHover}>
         <a href="/resume.pdf" target="_blank">
           <span className="hidden md:inline">Resume</span>
           <MdAttachment className="md:hidden" />
         </a>
       </motion.button>
-      <motion.button className="rounded-[0.6rem] shadow-md bg-slate-700 px-2.5 py-1 text-slate-300 border-t-1 border-t-slate-500/75 border-1 border-slate-700/75"
+      <motion.button className="rounded-[0.6rem] shadow-md bg-slate-700 px-2.5 py-1 text-slate-300 border-t-1 border-t-slate-500/60 border-1 border-slate-700/75"
                      whileHover={whileHover}>
         <a href="https://calendly.com/haidar47x/30min" className="">
           <span className="hidden md:inline">Let's talk</span>
