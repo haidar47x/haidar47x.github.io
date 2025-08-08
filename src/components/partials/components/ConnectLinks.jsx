6c322cd1f055fd177480aedc15f099c77b997f90
Haidar Ali <haidar47x@gmail.com>
2025-08-08 20:02:00 +0500

    Resume file -> Resume link

--- src/components/partials/components/ConnectLinks.jsx
+++ src/components/partials/components/ConnectLinks.jsx
@@ -1,6 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
-import { MdEmail } from "react-icons/md";
 import { MdAttachment } from "react-icons/md";
 import { SiGooglemeet } from "react-icons/si";
 
@@ -10,7 +9,7 @@ function ConnectLinks() {
     <div className="flex flex-row justify-center gap-3 md:gap-2 pr-1 md:p-0">
       <motion.button className="rounded-[0.6rem] shadow-md bg-black/5 dark:bg-[rgba(255,255,255,0.08)] px-2.5 py-1 backdrop-blur-[9.6px] border-t-slate-500/60 border-1 border-slate-700"
                      whileHover={whileHover}>
-        <a href="/resume.pdf" target="_blank">
+        <a href="https://www.overleaf.com/download/project/689382fa5ba48dcc612e711b/build/1988a330e97-b6658cf26a46c301/output/output.pdf?compileGroup=standard&clsiserverid=clsi-reg-n2d-c-f-ljl3&enable_pdf_caching=true&popupDownload=false" target="_blank">
           <span className="hidden md:inline">Resume</span>
           <MdAttachment className="md:hidden" />
         </a>
